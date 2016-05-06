ddb09570fbf0827ea9a091fcc5f04c02d26266b9
James Ravn <james.ravn@sky.uk>
2016-05-06 17:59:40 +0100

    Add initial ingress update logic

--- feed/lib/controller.go
+++ feed/lib/controller.go
@@ -1,6 +1,9 @@
 package feed
 
-import "github.com/golang/glog"
+import (
+	"github.com/golang/glog"
+	"github.com/sky-uk/umc-ingress/feed/k8s"
+)
 
 // Controller for Kubernetes ingress.
 type Controller interface {
@@ -9,19 +12,24 @@ type Controller interface {
 
 // LoadBalancer that the controller will modify.
 type LoadBalancer interface {
+	Update([]LoadBalancerEntry) error
 }
 
-// KubernetesClient for observing changes to the Kubernetes cluster.
-type KubernetesClient interface {
+// LoadBalancerEntry describes the ingress for a single host, path, and service.
+type LoadBalancerEntry struct {
+	Host        string
+	Path        string
+	ServiceName string
+	ServicePort int32
 }
 
 type impl struct {
 	lb     LoadBalancer
-	client KubernetesClient
+	client k8s.Client
 }
 
 // NewController creates a Controller.
-func NewController(loadBalancer LoadBalancer, kubernetesClient KubernetesClient) Controller {
+func NewController(loadBalancer LoadBalancer, kubernetesClient k8s.Client) Controller {
 	return &impl{
 		lb:     loadBalancer,
 		client: kubernetesClient,
@@ -30,6 +38,30 @@ func NewController(loadBalancer LoadBalancer, kubernetesClient KubernetesClient)
 
 // Run controller.
 func (c *impl) Run() error {
-	glog.Infof("hello %v and %v", c.lb, c.client)
-	return nil
+	glog.Infof("Starting controller for %v and %v", c.lb, c.client)
+
+	ingresses, err := c.client.GetIngresses()
+	if err != nil {
+		return err
+	}
+
+	var entries []LoadBalancerEntry
+	for _, ingress := range ingresses {
+		for _, rule := range ingress.Spec.Rules {
+			for _, path := range rule.HTTP.Paths {
+				entry := LoadBalancerEntry{
+					Host:        rule.Host,
+					Path:        path.Path,
+					ServiceName: path.Backend.ServiceName,
+					ServicePort: path.Backend.ServicePort.IntValue(),
+				}
+
+				entries = append(entries, entry)
+			}
+		}
+	}
+
+	glog.Infof("Updating load balancer for %d entries", len(entries))
+	err = c.lb.Update(entries)
+	return err
 }
