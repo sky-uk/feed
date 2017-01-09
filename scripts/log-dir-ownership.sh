d0bdd413ec23e795f9754c9a30b2b1bd076d1d1a
James Ravn <james@r-vn.org>
2017-01-09 15:29:46 +0000

    Don't fail if /var/log/nginx isn't pre-mounted

--- scripts/log-dir-ownership.sh
+++ scripts/log-dir-ownership.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+[ -f /var/log/nginx ] || mkdir -p /var/log/nginx
 chown nginx:nginx /var/log/nginx/
 # non group or public write required by logrotate
 chmod 755 /var/log/nginx/
