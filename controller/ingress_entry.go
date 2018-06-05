d7e344ff17e5c0de41d940addb778eb6d63b80c3
ashutoshgawande <Avanti26>
2018-06-05 12:55:00 +0100

    Corrected error message for scenario where service address is set to 'None'

--- controller/ingress_entry.go
+++ controller/ingress_entry.go
@@ -51,7 +51,7 @@ func (e IngressEntry) validate() error {
 		return errors.New("missing service address")
 	}
 	if e.ServiceAddress == "None" {
-		return errors.New("missing service address")
+		return errors.New("Service address is set to 'None' ignoring entry")
 	}
 	if e.ServicePort == 0 {
 		return errors.New("missing service port")
