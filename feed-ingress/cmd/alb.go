027a3f1c76fa8747285f31305ebf919dd3fee2f4
Howard Burgess <howard@bluesoup.co.uk>
2019-08-13 11:15:28 +0100

    Review comment

--- feed-ingress/cmd/alb.go
+++ feed-ingress/cmd/alb.go
@@ -21,11 +21,11 @@ Specifically, they don't respect the deregistration delay. As a result, we don't
 func init() {
 	rootCmd.AddCommand(albCmd)
 
-	elbCmd.Flags().StringVar(&region, "region", defaultRegion,
+	albCmd.Flags().StringVar(&region, "region", defaultRegion,
 		"AWS region for frontend attachment.")
-	elbCmd.Flags().StringSliceVar(&targetGroupNames, "alb-target-group-names", []string{},
+	albCmd.Flags().StringSliceVar(&targetGroupNames, "alb-target-group-names", []string{},
 		"Names of ALB target groups to attach to, separated by commas.")
-	elbCmd.Flags().DurationVar(&targetGroupDeregistrationDelay, "alb-target-group-deregistration-delay",
+	albCmd.Flags().DurationVar(&targetGroupDeregistrationDelay, "alb-target-group-deregistration-delay",
 		defaultTargetGroupDeregistrationDelay,
 		"Delay to wait for feed-ingress to deregister from the ALB target group on shutdown. Should match"+
 			" the target group setting in AWS.")
